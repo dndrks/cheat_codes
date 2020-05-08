a6167cfb3294887eb87e8622ae3b5fd50b504de3
dndrks <derks.dan@gmail.com>
2020-05-08 09:37:11 -0500

    start encoder actions refactor

--- lib/encoder_actions.lua
+++ lib/encoder_actions.lua
@@ -1,4 +1,7 @@
-encoder_actions = {}
+local encoder_actions = {}
+
+local ea = encoder_actions
+ea.sc = {}
 
 function encoder_actions.init(n,d)
   if n == 1 then
@@ -6,15 +9,7 @@ function encoder_actions.init(n,d)
       local id = page.loops_sel + 1
       if id ~= 4 then
         if key1_hold or grid.alt == 1 then
-          --if grid_pat[id].play == 0 and grid_pat[id].tightened_start == 0 and grid_pat[id].external_start == 0 then
-          if grid_pat[id].play == 0 and grid_pat[id].tightened_start == 0 then
-            bank[id].id = util.clamp(bank[id].id + d,1,16)
-            selected[id].x = (math.ceil(bank[id].id/4)+(5*(id-1)))
-            selected[id].y = 8-((bank[id].id-1)%4)
-            cheat(id,bank[id].id)
-          else
-            bank[id].focus_pad = util.clamp(bank[id].focus_pad + d,1,16)
-          end
+          ea.change_pad(id,d)
         else
           local which_pad = nil
           if bank[id].focus_hold == false then
@@ -22,42 +17,18 @@ function encoder_actions.init(n,d)
           else
             which_pad = bank[id].focus_pad
           end
-          local current_difference = (bank[id][which_pad].end_point - bank[id][which_pad].start_point)
-          if bank[id][which_pad].start_point + current_difference <= (9+(8*(bank[id][which_pad].clip-1))) then
-            bank[id][which_pad].start_point = util.clamp(bank[id][which_pad].start_point + d/loop_enc_resolution,(1+(8*(bank[id][which_pad].clip-1))),(9+(8*(bank[id][which_pad].clip-1))))
-            bank[id][which_pad].end_point = bank[id][which_pad].start_point + current_difference
-          else
-            bank[id][which_pad].end_point = (9+(8*(bank[id][which_pad].clip-1)))
-            bank[id][which_pad].start_point = bank[id][which_pad].end_point - current_difference
-          end
+          ea.move_play_window(bank[id][which_pad],d/loop_enc_resolution)
         end
         if bank[id].focus_hold == false then
-          softcut.loop_start(id+1,bank[id][bank[id].id].start_point)
-          softcut.loop_end(id+1,bank[id][bank[id].id].end_point)
+          ea.sc.move_play_window(id)
         end
       elseif id == 4 then
         if key1_hold or grid.alt == 1 then
-          local pre_adjust = rec.clip
-          local current_difference = (rec.end_point - rec.start_point)
-          rec.clip = util.clamp(rec.clip+d,1,3)
-          rec.start_point = rec.start_point - ((pre_adjust - rec.clip)*8)
-          rec.end_point = rec.start_point + current_difference
+          ea.change_buffer(rec,d)
         else
-          local current_difference = (rec.end_point - rec.start_point)
-          if d >=0 then
-            if rec.end_point + current_difference < (9+(8*(rec.clip-1))) then
-              rec.start_point = util.clamp(rec.start_point + current_difference * (d>0 and 1 or -1), (1+(8*(rec.clip-1))),(9+(8*(rec.clip-1))))
-              rec.end_point = rec.start_point + current_difference
-            end
-          else
-            if rec.end_point - current_difference > (1+(8*(rec.clip-1))) then
-              rec.end_point = util.clamp(rec.end_point + current_difference * (d>0 and 1 or -1), (1+(8*(rec.clip-1))),(9+(8*(rec.clip-1))))
-              rec.start_point = rec.end_point - current_difference
-            end
-          end
+          ea.move_rec_window(rec,d)
         end
-        softcut.loop_start(1,rec.start_point)
-        softcut.loop_end(1,rec.end_point-0.01)
+          ea.sc.move_rec_window(rec)
       end
     elseif menu == 6 then
       page.delay_sel = util.clamp(page.delay_sel+d,0,4)
@@ -72,40 +43,7 @@ function encoder_actions.init(n,d)
       local id = page.loops_sel + 1
       if id ~=4 then
         if key1_hold and grid.alt == 0 then
-          local focused_pad = nil
-          --if grid_pat[id].play == 0 and grid_pat[id].tightened_start == 0 and grid_pat[id].external_start == 0 then
-          if grid_pat[id].play == 0 and grid_pat[id].tightened_start == 0 then
-            focused_pad = bank[id].id
-          else
-            focused_pad = bank[id].focus_pad
-          end
-          local pre_adjust = bank[id][focused_pad].clip
-          local current_difference = (bank[id][focused_pad].end_point - bank[id][focused_pad].start_point)
-          if bank[id][focused_pad].mode == 1 and bank[id][focused_pad].clip + d > 3 then
-            bank[id][focused_pad].mode = 2
-            bank[id][focused_pad].clip = 1
-          elseif bank[id][focused_pad].mode == 2 and bank[id][focused_pad].clip + d < 1 then
-            bank[id][focused_pad].mode = 1
-            bank[id][focused_pad].clip = 3
-          else
-            bank[id][focused_pad].clip = util.clamp(bank[id][focused_pad].clip+d,1,3)
-          end
-          bank[id][focused_pad].start_point = bank[id][focused_pad].start_point - ((pre_adjust - bank[id][focused_pad].clip)*8)
-          bank[id][focused_pad].end_point = bank[id][focused_pad].start_point + current_difference
-          --if grid_pat[id].play == 0 and grid_pat[id].tightened_start == 0 and grid_pat[id].external_start == 0 then
-          if grid_pat[id].play == 0 and grid_pat[id].tightened_start == 0 then
-            cheat(id,bank[id].id)
-          end
-          if focused_pad == 16 then
-            for i = 1,15 do
-              local pre_adjust = bank[id][i].clip
-              bank[id][i].mode = bank[id][16].mode
-              bank[id][i].clip = bank[id][16].clip
-              local current_difference = (bank[id][i].end_point - bank[id][i].start_point)
-              bank[id][i].start_point = bank[id][i].start_point - ((pre_adjust - bank[id][i].clip)*8)
-              bank[id][i].end_point = bank[id][i].start_point + current_difference
-            end
-          end
+          ea.change_pad_clip(id,d)
         elseif key1_hold == false and grid.alt == 0 then
           local which_pad = nil
           if bank[id].focus_hold == false then
@@ -113,13 +51,9 @@ function encoder_actions.init(n,d)
           else
             which_pad = bank[id].focus_pad
           end
-          if d >= 0 and bank[id][which_pad].start_point < (bank[id][which_pad].end_point - d/loop_enc_resolution) then
-            bank[id][which_pad].start_point = util.clamp(bank[id][which_pad].start_point+d/loop_enc_resolution,(1+(8*(bank[id][which_pad].clip-1))),(8.9+(8*(bank[id][which_pad].clip-1))))
-          elseif d < 0 then
-            bank[id][which_pad].start_point = util.clamp(bank[id][which_pad].start_point+d/loop_enc_resolution,(1+(8*(bank[id][which_pad].clip-1))),(8.9+(8*(bank[id][which_pad].clip-1))))
-          end
+          ea.move_start(bank[id][which_pad],d/loop_enc_resolution)
           if bank[id].focus_hold == false then
-            softcut.loop_start(id+1, bank[id][bank[id].id].start_point)
+            ea.sc.move_start(id)
           end
         end
       elseif id == 4 then
@@ -195,7 +129,6 @@ function encoder_actions.init(n,d)
       if id ~= 4 then
         if key1_hold or grid.alt == 1 then
           local focused_pad = nil
-          --if grid_pat[id].play == 0 and grid_pat[id].tightened_start == 0 and grid_pat[id].external_start == 0 then
           if grid_pat[id].play == 0 and grid_pat[id].tightened_start == 0 then
             focused_pad = bank[id].id
           else
@@ -207,7 +140,6 @@ function encoder_actions.init(n,d)
             current_offset = 0
           end
           bank[id][focused_pad].offset = math.pow(0.5, -current_offset / 12)
-          --if grid_pat[id].play == 0 and grid_pat[id].tightened_start == 0 and grid_pat[id].external_start == 0 then
           if grid_pat[id].play == 0 and grid_pat[id].tightened_start == 0 then
             cheat(id,bank[id].id)
           end
@@ -456,4 +388,134 @@ function encoder_actions.init(n,d)
   redraw()
 end
 
+function ea.move_play_window(target,delta)
+  local current_difference = (target.end_point - target.start_point)
+  local current_clip = 8*(target.clip-1)
+  if target.start_point + current_difference <= 9+current_clip then
+    target.start_point = util.clamp(target.start_point + delta, 1+current_clip, 9+current_clip)
+    target.end_point = target.start_point + current_difference
+  else
+    target.end_point = (9+current_clip)
+    target.start_point = target.end_point - current_difference
+  end
+end
+
+function ea.move_rec_window(target,delta)
+  local current_difference = (target.end_point - target.start_point)
+  local current_clip = 8*(target.clip-1)
+  if delta >=0 then
+    if target.end_point + current_difference < (9+(8*current_clip)) then
+      target.start_point = util.clamp(target.start_point + current_difference * (delta>0 and 1 or -1), (1+(8*current_clip)),(9+(8*current_clip)))
+      target.end_point = target.start_point + current_difference
+    end
+  else
+    if target.end_point - current_difference > (1+(8*current_clip)) then
+      target.end_point = util.clamp(target.end_point + current_difference * (delta>0 and 1 or -1), (1+(8*current_clip)),(9+(8*current_clip)))
+      target.start_point = target.end_point - current_difference
+    end
+  end
+end
+
+function ea.change_pad(target,delta)
+  pad = bank[target]
+  if grid_pat[target].play == 0 and grid_pat[target].tightened_start == 0 then
+    pad.id = util.clamp(pad.id + delta,1,16)
+    selected[target].x = (math.ceil(pad.id/4)+(5*(target-1)))
+    selected[target].y = 8-((pad.id-1)%4)
+    cheat(target,pad.id)
+  else
+    pad.focus_pad = util.clamp(pad.focus_pad + delta,1,16)
+  end
+end
+
+function ea.change_buffer(target,delta)
+  local pre_adjust = target.clip
+  local current_difference = (target.end_point - target.start_point)
+  target.clip = util.clamp(target.clip+delta,1,3)
+  target.start_point = target.start_point - ((pre_adjust - target.clip)*8)
+  target.end_point = target.start_point + current_difference
+end
+
+function ea.change_pad_clip(target,delta)
+  local focused_pad = nil
+  if grid_pat[target].play == 0 and grid_pat[target].tightened_start == 0 then
+    focused_pad = bank[target].id
+  else
+    focused_pad = bank[target].focus_pad
+  end
+  pad = bank[target][focused_pad]
+  local pre_adjust = pad.clip
+  local current_difference = (pad.end_point - pad.start_point)
+  if pad.mode == 1 and pad.clip + delta > 3 then
+    pad.mode = 2
+    pad.clip = 1
+  elseif pad.mode == 2 and pad.clip + delta < 1 then
+    pad.mode = 1
+    pad.clip = 3
+  else
+    pad.clip = util.clamp(pad.clip+delta,1,3)
+  end
+  pad.start_point = pad.start_point - ((pre_adjust - pad.clip)*8)
+  pad.end_point = pad.start_point + current_difference
+  if grid_pat[target].play == 0 and grid_pat[target].tightened_start == 0 then
+    cheat(target,bank[target].id)
+  end
+  if focused_pad == 16 then
+    for i = 1,15 do
+      local pre_adjust = bank[target][i].clip
+      bank[target][i].mode = bank[target][16].mode
+      bank[target][i].clip = bank[target][16].clip
+      local current_difference = (bank[target][i].end_point - bank[target][i].start_point)
+      bank[target][i].start_point = bank[target][i].start_point - ((pre_adjust - bank[target][i].clip)*8)
+      bank[target][i].end_point = bank[target][i].start_point + current_difference
+    end
+  end
+end
+
+function ea.move_start(target,delta)
+  if delta >= 0 and target.start_point < (target.end_point - delta) then
+    target.start_point = util.clamp(target.start_point+delta,(1+(8*(target.clip-1))),(8.9+(8*(target.clip-1))))
+  elseif d < 0 then
+    target.start_point = util.clamp(target.start_point+delta,(1+(8*(target.clip-1))),(8.9+(8*(target.clip-1))))
+  end
+end
+
+function ea.sc.move_play_window(target)
+  pad = bank[target][bank[target].id]
+  softcut.loop_start(target+1,pad.start_point)
+  softcut.loop_end(target+1,pad.end_point)
+end
+
+function ea.sc.move_rec_window(target)
+  softcut.loop_start(1,target.start_point)
+  softcut.loop_end(1,target.end_point-0.01)
+end
+
+function ea.sc.move_start(target)
+  pad = bank[target][bank[target].id]
+  softcut.loop_start(target+1, pad.start_point)
+end
+
 return encoder_actions
+
+--[===[
+
+local encoder_actions = {}
+ea = encoder_actions
+
+ea.sc = {}
+
+ea.init(n,d)
+  -- TODO
+  local pattern_playing = false
+  if grid_pat[id].play == 0 and grid_pat[id].tightened_start == 0 then
+    pattern_playing = true
+  else
+    pattern_playing = false
+  end
+
+    
+end
+
+
+--]===]
